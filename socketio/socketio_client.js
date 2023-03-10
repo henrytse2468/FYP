c1f9caf997050b02eb748938b93d0fd6e0c5da06
CKMFYP <114097303+CKMFYP@users.noreply.github.com>
2023-03-10 11:52:10 +0800

    Update socketio_client.js

--- socketio/socketio_client.js
+++ socketio/socketio_client.js
@@ -1,6 +1,17 @@
 // ES6 import or TypeScript
 // import { io } from "socket.io-client";
 // CommonJS
+
+var room_num;
+var floor;
+
+room_num = process.argv[2];
+floor = process.argv[3];
+
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
 const io = require("socket.io-client");
 
 const socket = io("http://localhost:3000");
@@ -10,19 +21,28 @@ socket.on("connect", () => {
   
   if(socket.connected){
     console.log('connected. ID is ' + socket.id);
-    socket.emit("location", "2");
-    // socket.disconnect();
+    socket.emit("location", room_num);
+    
   }
 });
 
-socket.once("first response", (response)=>{
-  console.log('first response is '+response);
-  socket.emit("point", "first");
+socket.once('location_ok', (response)=>{
+  console.log(response);
+  socket.emit('action', 'evelator');
 });
 
-socket.once("second response", (response)=>{
-  console.log('second response is '+response);
-  socket.emit("point", "second");
+socket.on("response",(args)=>{
+    if (args['stage']=='evelator') {
+      console.log(args.stage + ' is ' + args.status);
+      socket.emit("action", "toFloor");
+
+    } else if(args['stage']=='floorX') {
+        console.log(args.stage + ' is ' + args.status);
+        socket.emit('action', 'toRoomX');
+    }
+});
+
+socket.once('finished', (response)=>{
+  console.log(response + ', now disconnecting...');
   socket.disconnect();
 });
-
