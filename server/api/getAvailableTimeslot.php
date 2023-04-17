4b674403c1aab3e299fc80f5f48b66b913fba0fa
CKMFYP <114097303+CKMFYP@users.noreply.github.com>
2023-04-17 15:05:35 +0800

    Add files via upload

---
+++ server/api/getAvailableTimeslot.php
@@ -0,0 +1,24 @@
+<?php
+    ini_set('display_errors', 1);
+    $hostname = "localhost:3306";
+    $username = "root";
+    $pwd = "Ming@12321";
+    $db = "testing";
+    $conn = mysqli_connect($hostname, 
+            $username, $pwd, $db) 
+            or die(mysqli_connect_error());
+
+    $sql = "SELECT start_time FROM test WHERE status = 'available'";
+    $rs = mysqli_query($conn, $sql);
+
+    $emparray = array();
+   
+    while($rc = mysqli_fetch_assoc($rs)) {
+        $emparray[] = $rc;
+        
+    };
+    
+    echo json_encode($emparray);
+    
+    mysqli_close($conn);
+?>
